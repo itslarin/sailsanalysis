acd1b5b8ace18cceed38e1d398a0a495aad5eb90
Alexander Larin <itslarin@gmail.com>
2019-07-14 12:18:35 +0300

    changes

--- modules/dbfuncs.py
+++ modules/dbfuncs.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import psycopg2
+#import psycopg2
    
    
 def dbGetEngine(host,port,database,user,password):
     #return create_engine('postgresql+psycopg2://weukgmth:EjnV3ZckgHVsF9P8uTRO-WkmESwBTzOB@manny.db.elephantsql.com:5432/weukgmth')    
-    return create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")    
+    #return create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")    
+	return create_engine(f"postgresql://{user}:{password}@{host}:{port}/{database}",pool_size=1, max_overflow=0)    
     
     
 def dbGetSession(engine):
